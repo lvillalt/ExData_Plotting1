74e104175bc404eac734cb017ecef96e00dd484b
lvillalt <lvillalt@gmail.com>
2014-08-07 21:53:10 -0400

    Added comments to R scripts
    Final version

--- plot3.R
+++ plot3.R
@@ -1,19 +1,39 @@
+## Code for plot3
+## Can be run using source("plot3.R") if the file is in current working directory
+
+## We first read the *.csv file. (Note that the separator is a semicolon)
+##
+## The colClasses parameter is used here to make sure that the date and time
+## columns are read as character type, so that we can be ready to convert to
+## DateTime objects
+
 	household <- read.csv(file="household_power_consumption.txt", sep=";", 
 		colClasses = c("character", "character", "numeric", "numeric","numeric",
 		"numeric","numeric", "numeric", "numeric" ), na.strings="?")
-	
+
+## We now convert the Date and Time columns into the proper format	
+		
 	DateTime <- paste(household$Date, household$Time)
 	DateTime <- strptime(DateTime, "%d/%m/%Y %H:%M:%S")
 	
+## We create a new object for the DateTime format		
+	
 	household.DateTime <- cbind(DateTime, household)
+
+## And eliminate the original columns	
 	
 	household.DateTime$Date <- NULL
 	household.DateTime$Time <- NULL
 
+## Subset the data to be within the dates specified			
+	
 	household_plot_data <- household.DateTime[(
 		(household.DateTime$DateTime>"2007-01-31 23:59:00")
 		&(household.DateTime$DateTime<"2007-02-03 00:00:00")),]
-		
+
+## We finally plot the annotated time-series plot to a *.png file	
+## Note how additional lines are added with the lines function
+##  and line color is changed with parameters there. A legend is added.			
 
 	png(filename="plot3.png", width=480, height=480)
 	
--- plot4.R
+++ plot4.R
@@ -1,7 +1,41 @@
+## Code for plot4
+## Can be run using source("plot4.R") if the file is in current working directory
+
+## We first read the *.csv file. (Note that the separator is a semicolon)
+##
+## The colClasses parameter is used here to make sure that the date and time
+## columns are read as character type, so that we can be ready to convert to
+## DateTime objects
+
+	household <- read.csv(file="household_power_consumption.txt", sep=";", 
+		colClasses = c("character", "character", "numeric", "numeric","numeric",
+		"numeric","numeric", "numeric", "numeric" ), na.strings="?")
+
+## We now convert the Date and Time columns into the proper format	
+		
+	DateTime <- paste(household$Date, household$Time)
+	DateTime <- strptime(DateTime, "%d/%m/%Y %H:%M:%S")
+
+## We create a new object for the DateTime format	
+	
+	household.DateTime <- cbind(DateTime, household)
+		
+## And eliminate the original columns		
+	
+	household.DateTime$Date <- NULL
+	household.DateTime$Time <- NULL
+	
+## Subset the data to be within the dates specified			
 
 	household_plot_data <- household.DateTime[(
 		(household.DateTime$DateTime>"2007-01-31 23:59:00")
 		&(household.DateTime$DateTime<"2007-02-03 00:00:00")),]
+		
+		
+## We finally plot the multi-plot time-series plot to a *.png file	
+## Note how the previous plot code is used, with some minor mods to
+## match the expected output. Note the bty="n" in the legend to 
+## remove the box, and the xlab="datetime", among other changes	
 		
 	png(filename="plot4.png", width=480, height=480)
 	
